9c783835f8cfd683b0ae96f273a67d4f5e6c0d41
Mike Bostock <mbostock@gmail.com>
2021-09-06 12:26:46 -0700

    extract regexp; use non-capturing groups

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,5 @@
+const re = /^(?:[-+]\d{2})?\d{4}(?:-\d{2}(?:-\d{2})?)?(?:T\d{2}:\d{2}(?::\d{2}(?:\.\d{3})?)?(?:Z|[-+]\d{2}:?\d{2})?)?$/;
+
 export default function parse(string) {
-  return /^([-+]\d{2})?\d{4}(-\d{2}(-\d{2})?)?(T\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:?\d{2})?)?$/.test(string += "")
-    ? new Date(string)
-    : null;
+  return re.test(string += "") ? new Date(string) : null;
 }
